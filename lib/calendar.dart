9cc3b3dc093da9bee7a99070c801005ba63f9ddf
wnal4634 <wnal4634@naver.com>
2023-05-20 23:33:02 +0900

    시도, 취소

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,157 +1,157 @@
-import 'package:flutter/material.dart';
-import 'package:note_calendar/event.dart';
-import 'package:table_calendar/table_calendar.dart';
+// import 'package:flutter/material.dart';
+// import 'package:note_calendar/event.dart';
+// import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatefulWidget {
-  const Calendar({super.key});
+// class Calendar extends StatefulWidget {
+//   const Calendar({super.key});
 
-  @override
-  State<Calendar> createState() => _CalendarState();
-}
+//   @override
+//   State<Calendar> createState() => _CalendarState();
+// }
 
-class _CalendarState extends State<Calendar> {
-  Map<DateTime, List<Event>> selectedEvents = {};
-  CalendarFormat format = CalendarFormat.month;
-  DateTime selectedDay = DateTime.now();
-  DateTime focusedDay = DateTime.now();
-  final TextEditingController _eventController = TextEditingController();
+// class _CalendarState extends State<Calendar> {
+//   Map<DateTime, List<Event>> selectedEvents = {};
+//   CalendarFormat format = CalendarFormat.month;
+//   DateTime selectedDay = DateTime.now();
+//   DateTime focusedDay = DateTime.now();
+//   final TextEditingController _eventController = TextEditingController();
 
-  @override
-  void initState() {
-    selectedEvents = {};
-    super.initState();
-  }
+//   @override
+//   void initState() {
+//     selectedEvents = {};
+//     super.initState();
+//   }
 
-  List<Event> _getEventsfromDay(DateTime date) {
-    return selectedEvents[date] ?? [];
-  }
+//   List<Event> _getEventsfromDay(DateTime date) {
+//     return selectedEvents[date] ?? [];
+//   }
 
-  @override
-  void dispose() {
-    _eventController.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     _eventController.dispose();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('ㅙㅙ'),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          TableCalendar(
-            locale: 'ko_KR',
-            focusedDay: selectedDay,
-            firstDay: DateTime(1990),
-            lastDay: DateTime(2050),
-            calendarFormat: format,
-            onFormatChanged: (CalendarFormat format) {
-              setState(() {
-                this.format = format;
-              });
-            },
-            startingDayOfWeek: StartingDayOfWeek.sunday,
-            daysOfWeekVisible: true,
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('ㅙㅙ'),
+//         centerTitle: true,
+//       ),
+//       body: Column(
+//         children: [
+//           TableCalendar(
+//             locale: 'ko_KR',
+//             focusedDay: selectedDay,
+//             firstDay: DateTime(1990),
+//             lastDay: DateTime(2050),
+//             calendarFormat: format,
+//             onFormatChanged: (CalendarFormat format) {
+//               setState(() {
+//                 this.format = format;
+//               });
+//             },
+//             startingDayOfWeek: StartingDayOfWeek.sunday,
+//             daysOfWeekVisible: true,
 
-            // 날짜 변경
-            onDaySelected: (DateTime selectDay, DateTime focusDay) {
-              setState(() {
-                selectedDay = selectDay;
-                focusedDay = focusDay;
-              });
-            },
+//             // 날짜 변경
+//             onDaySelected: (DateTime selectDay, DateTime focusDay) {
+//               setState(() {
+//                 selectedDay = selectDay;
+//                 focusedDay = focusDay;
+//               });
+//             },
 
-            eventLoader: _getEventsfromDay,
+//             eventLoader: _getEventsfromDay,
 
-            // 캘린더 디자인
-            calendarStyle: const CalendarStyle(
-              isTodayHighlighted: true,
-              selectedDecoration: BoxDecoration(
-                color: Colors.blueGrey,
-                shape: BoxShape.circle,
-              ),
-              selectedTextStyle: TextStyle(
-                // 선택한 날짜
-                color: Colors.white,
-              ),
-              todayDecoration: BoxDecoration(
-                // 현재 날짜
-                color: Colors.blue,
-                shape: BoxShape.circle,
-              ),
-              defaultDecoration: BoxDecoration(
-                shape: BoxShape.circle,
-              ),
-              weekendDecoration: BoxDecoration(
-                shape: BoxShape.circle,
-              ),
-            ),
-            selectedDayPredicate: (DateTime date) {
-              return isSameDay(selectedDay, date);
-            },
-            headerStyle: HeaderStyle(
-              formatButtonVisible: true, // 단위 포맷버튼 유무
-              titleCentered: true,
-              formatButtonShowsNext: false,
-              formatButtonDecoration: BoxDecoration(
-                // 포맷버튼 디자인
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              formatButtonTextStyle: const TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ),
-          ..._getEventsfromDay(selectedDay).map(
-            (Event event) => ListTile(
-              title: Text(event.title),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: const Text('Add Event'),
-            content: TextFormField(
-              controller: _eventController,
-            ),
-            actions: [
-              TextButton(
-                child: const Text('Cancel'),
-                onPressed: () => Navigator.pop(context),
-              ),
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  if (_eventController.text.isEmpty) {
-                  } else {
-                    if (selectedEvents[selectedDay] != null) {
-                      selectedEvents[selectedDay]?.add(
-                        Event(title: _eventController.text),
-                      );
-                    } else {
-                      selectedEvents[selectedDay] = [
-                        Event(title: _eventController.text)
-                      ];
-                    }
-                  }
-                  Navigator.pop(context);
-                  _eventController.clear();
-                  setState(() {});
-                  return;
-                },
-              )
-            ],
-          ),
-        ),
-        label: const Text('Add Event'),
-        icon: const Icon(Icons.add),
-      ),
-    );
-  }
-}
+//             // 캘린더 디자인
+//             calendarStyle: const CalendarStyle(
+//               isTodayHighlighted: true,
+//               selectedDecoration: BoxDecoration(
+//                 color: Colors.blueGrey,
+//                 shape: BoxShape.circle,
+//               ),
+//               selectedTextStyle: TextStyle(
+//                 // 선택한 날짜
+//                 color: Colors.white,
+//               ),
+//               todayDecoration: BoxDecoration(
+//                 // 현재 날짜
+//                 color: Colors.blue,
+//                 shape: BoxShape.circle,
+//               ),
+//               defaultDecoration: BoxDecoration(
+//                 shape: BoxShape.circle,
+//               ),
+//               weekendDecoration: BoxDecoration(
+//                 shape: BoxShape.circle,
+//               ),
+//             ),
+//             selectedDayPredicate: (DateTime date) {
+//               return isSameDay(selectedDay, date);
+//             },
+//             headerStyle: HeaderStyle(
+//               formatButtonVisible: true, // 단위 포맷버튼 유무
+//               titleCentered: true,
+//               formatButtonShowsNext: false,
+//               formatButtonDecoration: BoxDecoration(
+//                 // 포맷버튼 디자인
+//                 color: Colors.blue,
+//                 borderRadius: BorderRadius.circular(5.0),
+//               ),
+//               formatButtonTextStyle: const TextStyle(
+//                 color: Colors.white,
+//               ),
+//             ),
+//           ),
+//           ..._getEventsfromDay(selectedDay).map(
+//             (Event event) => ListTile(
+//               title: Text(event.title),
+//             ),
+//           ),
+//         ],
+//       ),
+//       floatingActionButton: FloatingActionButton.extended(
+//         onPressed: () => showDialog(
+//           context: context,
+//           builder: (context) => AlertDialog(
+//             title: const Text('Add Event'),
+//             content: TextFormField(
+//               controller: _eventController,
+//             ),
+//             actions: [
+//               TextButton(
+//                 child: const Text('Cancel'),
+//                 onPressed: () => Navigator.pop(context),
+//               ),
+//               TextButton(
+//                 child: const Text('OK'),
+//                 onPressed: () {
+//                   if (_eventController.text.isEmpty) {
+//                   } else {
+//                     if (selectedEvents[selectedDay] != null) {
+//                       selectedEvents[selectedDay]?.add(
+//                         Event(title: _eventController.text),
+//                       );
+//                     } else {
+//                       selectedEvents[selectedDay] = [
+//                         Event(title: _eventController.text)
+//                       ];
+//                     }
+//                   }
+//                   Navigator.pop(context);
+//                   _eventController.clear();
+//                   setState(() {});
+//                   return;
+//                 },
+//               )
+//             ],
+//           ),
+//         ),
+//         label: const Text('Add Event'),
+//         icon: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
